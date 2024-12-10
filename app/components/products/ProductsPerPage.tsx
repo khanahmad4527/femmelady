d9ffece6c13d6c82ff354f2d02cee194718ba980
khanahmad4527 <khanahmad4527@gmail.com>
2024-12-10 09:03:46 +0530

    added searchParams in ProductsPerPage

--- app/components/products/ProductsPerPage.tsx
+++ app/components/products/ProductsPerPage.tsx
@@ -1,5 +1,5 @@
 import { Select } from '@mantine/core';
-import { LANGUAGE_TO_LOCALE_LANGUAGE } from '~/constant';
+import { useSearchParams } from '@remix-run/react';
 import useCurrentLanguage from '~/hooks/useCurrentLanguage';
 import useTranslation from '~/hooks/useTranslation';
 import useUserLocale from '~/hooks/useUserLocale';
@@ -7,10 +7,12 @@ import useUserLocale from '~/hooks/useUserLocale';
 const ProductsPerPage = () => {
   const t = useTranslation();
   const { currentLanguage } = useCurrentLanguage();
-
   const userLocale = useUserLocale(currentLanguage);
+  const [searchParams, setSearchParams] = useSearchParams();
 
   const formatNumber = (value: number) => value.toLocaleString(userLocale);
+
+  const limitValue = searchParams.get('limit') ?? '10';
 
   const data = [
     { value: '10', label: formatNumber(10) },
@@ -19,13 +21,23 @@ const ProductsPerPage = () => {
     { value: '100', label: formatNumber(100) }
   ];
 
-  console.log(new Intl.NumberFormat('ar-AR').format(123456)); // Should display "١٢٣٤٥٦"
-  console.log(new Intl.NumberFormat('en-US').format(123456)); // Should display "123,456"
+  const handleFilterChange = (value: string | null) => {
+    const newFilter = value || null;
+    const newSearchParams = new URLSearchParams(searchParams);
+    if (newFilter) {
+      newSearchParams.set('limit', newFilter);
+    } else {
+      newSearchParams.delete('limit');
+    }
+    setSearchParams(newSearchParams, { preventScrollReset: true });
+  };
 
   return (
     <Select
       label={t('products.productsPerPage')}
+      defaultValue={limitValue}
       data={data}
+      onChange={handleFilterChange}
       clearable
       allowDeselect
     />
