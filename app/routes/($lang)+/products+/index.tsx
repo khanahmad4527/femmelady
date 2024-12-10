c4d0cc4a1529885829652f01e65cd8c7ca9f0fc5
khanahmad4527 <khanahmad4527@gmail.com>
2024-12-10 09:11:39 +0530

    updated Products page UI

--- app/routes/($lang)+/products+/index.tsx
+++ app/routes/($lang)+/products+/index.tsx
@@ -1,17 +1,26 @@
-import { Box, Group } from '@mantine/core';
+import { Box, Group, Stack } from '@mantine/core';
 import ProductsFilterBy from '~/components/products/ProductsFilterBy';
 import ProductsPerPage from '~/components/products/ProductsPerPage';
 import ProductsSortBy from '~/components/products/ProductsSortBy';
 
 const Products = () => {
   return (
-    <Box>
-      <Group>
-        <ProductsPerPage />
-        <ProductsSortBy />
-      </Group>
-      <ProductsFilterBy />
-    </Box>
+    <Stack>
+      <Box ml={{ base: 'none', md: 'auto' }}>
+        <Box display={{ base: 'block', md: 'none' }}>
+          <ProductsFilterBy />
+        </Box>
+
+        <Group>
+          <ProductsPerPage />
+          <ProductsSortBy />
+        </Group>
+      </Box>
+
+      <Box display={{ base: 'none', md: 'block' }}>
+        <ProductsFilterBy />
+      </Box>
+    </Stack>
   );
 };
 
