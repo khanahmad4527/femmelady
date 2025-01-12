e521a8701d9f3dceeaaf29cc93981bc8dce55b1c
khanahmad4527 <khanahmad4527@gmail.com>
2025-01-12 11:52:08 +0530

    feat: enhance HeaderCart component; add empty cart state with localized messages and navigation button

--- app/components/cart/HeaderCart.tsx
+++ app/components/cart/HeaderCart.tsx
@@ -1,6 +1,11 @@
-import { Drawer } from '@mantine/core';
+import { Box, Button, Drawer, Stack, Text } from '@mantine/core';
 import useTranslation from '~/hooks/useTranslation';
 import InfiniteCartLoader from './InfiniteCartLoader';
+import useHeaderFooterContext from '~/hooks/useHeaderFooterContext';
+import { IconDatabaseExclamation } from '~/icons';
+import { Link } from 'react-router';
+import { buildLocalizedLink } from '~/utils';
+import useCurrentLanguage from '~/hooks/useCurrentLanguage';
 
 const HeaderCart = ({
   opened,
@@ -10,6 +15,8 @@ const HeaderCart = ({
   close: () => void;
 }) => {
   const t = useTranslation();
+  const { cartCount } = useHeaderFooterContext();
+  const { currentLanguage } = useCurrentLanguage();
 
   return (
     <Drawer
@@ -22,7 +29,32 @@ const HeaderCart = ({
       title={t('checkout.shoppingBag')}
       pos={'relative'}
     >
-      <InfiniteCartLoader close={close}/>
+      {!cartCount ? (
+        <Stack align={'center'} gap={0}>
+          <Box ta={'center'} w={{ base: 50, md: 100 }}>
+            <IconDatabaseExclamation size={'100%'} />
+          </Box>
+          <Text ta={'center'} fz={25} fw={500} c={'primary'}>
+            {t('cart.emptyCartTitle')}
+          </Text>
+          <Text ta={'center'} fz={16} c={'primary'}>
+            {t('cart.emptyCartMessage')}
+          </Text>
+          <Button
+            mt={'md'}
+            component={Link}
+            to={buildLocalizedLink({
+              currentLanguage,
+              paths: ['products?force-validate=global']
+            })}
+            onClick={close}
+          >
+            {t('common.cartEmptyMessage')}
+          </Button>
+        </Stack>
+      ) : (
+        <InfiniteCartLoader close={close} />
+      )}
     </Drawer>
   );
 };
