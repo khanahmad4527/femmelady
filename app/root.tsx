bf7fe8d748f5783e645eae72b0a169e1a76d397c
khanahmad4527 <khanahmad4527@gmail.com>
2025-02-14 17:31:51 +0530

    feat: add robots meta tag to prevent indexing and update robots.txt to disallow all user agents

--- app/root.tsx
+++ app/root.tsx
@@ -118,6 +118,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="robots" content="noindex, nofollow" />{' '}
+        {/* Prevents indexing */}
         <Meta />
         <Links />
         <ColorSchemeScript />
--- app/routes/[robots.txt].tsx
+++ app/routes/[robots.txt].tsx
@@ -1,10 +1,23 @@
+// export const loader = () => {
+//   const robotText = `
+//           User-agent: Googlebot
+//           Disallow: /nogooglebot/
+//           User-agent: *
+//           Allow: /
+//           Sitemap: ${process.env?.APP_DOMAIN}/sitemap.xml`;
+
+//   return new Response(robotText, {
+//     status: 200,
+//     headers: {
+//       'Content-Type': 'text/plain'
+//     }
+//   });
+// };
 export const loader = () => {
   const robotText = `
-          User-agent: Googlebot
-          Disallow: /nogooglebot/
-          User-agent: *
-          Allow: /
-          Sitemap: ${process.env?.APP_DOMAIN}/sitemap.xml`;
+    User-agent: *
+    Disallow: /
+  `;
 
   return new Response(robotText, {
     status: 200,
